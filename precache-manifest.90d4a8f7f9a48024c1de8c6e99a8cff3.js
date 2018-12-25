455635c869abdcdb82dad206e3f53cbe03e192a3
Barpel <42574095+Barpel@users.noreply.github.com>
2018-12-25 18:00:59 +0200

    Updates

---
+++ precache-manifest.90d4a8f7f9a48024c1de8c6e99a8cff3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5fc96ca42dca61e235f",
+    "url": "/mister-bitcoin/static/css/main.56d6f309.chunk.css"
+  },
+  {
+    "revision": "b5fc96ca42dca61e235f",
+    "url": "/mister-bitcoin/static/js/main.b5fc96ca.chunk.js"
+  },
+  {
+    "revision": "12fc34ebc3e31faaa514",
+    "url": "/mister-bitcoin/static/js/1.12fc34eb.chunk.js"
+  },
+  {
+    "revision": "e0b16d0559dbb2c784f3",
+    "url": "/mister-bitcoin/static/js/runtime~main.e0b16d05.js"
+  },
+  {
+    "revision": "e3f188d83b4cec2ca4bfc562b2f8ea90",
+    "url": "/mister-bitcoin/index.html"
+  }
+];
